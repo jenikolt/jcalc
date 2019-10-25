8df64beff97a045ef3a9293a48d99e180456ecf5
Nikolay Nosak <jenikolt@yandex.ru>
2019-10-25 15:56:48 +0400

    add files

---
+++ jcalc.js
@@ -0,0 +1,27 @@
+const str_screen = document.getElementById("str-screen");
+const result_screen = document.getElementById("result-screen");
+const btn_panel = document.getElementById("btn-panel");
+
+
+const modStrScreen = (str, action) => {
+    if (action === 'clear') {
+        str_screen.innerHTML = '';
+        return;
+    }
+    if (action === 'bck') {
+        
+    }
+}
+
+const dispResult = (result) => {
+
+}
+
+const runCalc = (str) => {
+
+}
+
+
+const buttonClick = (e) => {
+    return
+}
